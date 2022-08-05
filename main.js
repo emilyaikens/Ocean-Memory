9b195f7b2b32d3ebf7f8bb0d3e0a96c5716278dc
emilyaikens <emily.c.aikens@gmail.com>
2022-08-05 16:55:11 -0700

    js create dom selections and game variables

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
-const cards = document.querySelectorAll('.card');
+let domSelect = {
+    cards: document.querySelectorAll('.card'),
+    timer: document.getElementById("time-left"),
+    matches: document.getElementById("matches"),
+    directions: document.getElementById("directions")
+};
+let gameVariables = {
+    matches: 0,
+    firstSelect: "null",
+    secondSelect: "null",
+};
 
-cards.forEach(function(card) {
-    card.addEventListener('click', function() {
-        card.classList.toggle('flipped');
-    })
-});
+function flipCards() {
+    domSelect.cards.forEach(function(card) {
+        card.addEventListener('click', function() {
+            card.classList.toggle('flipped');
+         })
+    });
+};
+
+flipCards();
