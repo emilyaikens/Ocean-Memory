9dff3aabbd1e7f54de2695d2a5a68fc38f303538
emilyaikens <emily.c.aikens@gmail.com>
2022-08-06 10:17:37 -0700

    get innerHTML of .card-front, add conditions for first/second click, skeleton for match and timer functions

--- main.js
+++ main.js
@@ -4,18 +4,40 @@ let domSelect = {
     matches: document.getElementById("matches"),
     directions: document.getElementById("directions")
 };
-let gameVariables = {
+let gameVars = {
     matches: 0,
+    totalClicks: 0,
     firstSelect: "null",
     secondSelect: "null",
 };
 
-function flipCards() {
+//click and flip card
     domSelect.cards.forEach(function(card) {
         card.addEventListener('click', function() {
             card.classList.toggle('flipped');
          })
     });
-};
 
-flipCards();
+    domSelect.cards.forEach(function(card) {
+        card.addEventListener('click', function() {
+           let cardFront = card.firstElementChild.innerHTML; //get inner html of .card-front of clicked card
+           gameVars.totalClicks = gameVars.totalClicks + 1;
+           if (gameVars.firstSelect === "null") {
+             gameVars.firstSelect = cardFront;
+           } else if (gameVars.firstSelect !== "null" && gameVars.secondSelect === "null") {
+            gameVars.secondSelect = cardFront;
+           };
+           console.log(gameVars.totalClicks);
+         });
+    });
+
+if (gameVars.firstSelect !== "null" && gameVars.secondSelect !== "null") {
+    //check if match
+    //turn cards back around (on timer)
+}
+
+if (gameVars.totalClicks === 1) {
+    //start timer
+}
+
+//if timer === 0, end game 
