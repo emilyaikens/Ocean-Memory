952ccb0399a887ce91800225f6e8967d76a50a16
emilyaikens <emily.c.aikens@gmail.com>
2022-08-06 10:41:16 -0700

    start check for matches conditions and functions

--- main.js
+++ main.js
@@ -8,36 +8,49 @@ let gameVars = {
     matches: 0,
     totalClicks: 0,
     firstSelect: "null",
-    secondSelect: "null",
+    secondSelect: "null"
 };
-
+initiate();
 //click and flip card
+function initiate () {
     domSelect.cards.forEach(function(card) {
         card.addEventListener('click', function() {
             card.classList.toggle('flipped');
          })
     });
-
+};
     domSelect.cards.forEach(function(card) {
         card.addEventListener('click', function() {
            let cardFront = card.firstElementChild.innerHTML; //get inner html of .card-front of clicked card
-           gameVars.totalClicks = gameVars.totalClicks + 1;
-           if (gameVars.firstSelect === "null") {
+           gameVars.totalClicks = gameVars.totalClicks + 1; //count total clicks
+           if (gameVars.firstSelect === "null") { //player selections
              gameVars.firstSelect = cardFront;
            } else if (gameVars.firstSelect !== "null" && gameVars.secondSelect === "null") {
             gameVars.secondSelect = cardFront;
            };
            console.log(gameVars.totalClicks);
+           console.log(gameVars.secondSelect);
+           console.log(gameVars.firstSelect);
          });
     });
 
-if (gameVars.firstSelect !== "null" && gameVars.secondSelect !== "null") {
-    //check if match
+if (gameVars.firstSelect !== "null" && gameVars.secondSelect !== "null") { //work on this!
+    if (gameVars.firstSelect === gameVars.secondSelect) {
+        gameVars.matches = gameVars.matches + 1;
+        console.log(gameVars.matches);
+        domSelect.matches.innerHTML = ("matches: " + gameVars.matches);
+        //can't click on matched tiles anymore
+    }
     //turn cards back around (on timer)
-}
-
+};
 if (gameVars.totalClicks === 1) {
     //start timer
 }
 
 //if timer === 0, end game 
+
+//check winner
+if (gameVars.matches === "8") {
+    //display win message
+    //stop timer
+}
