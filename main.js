2e91dc87c72988e035a1fa9cc800aad75ae6a6b1
emilyaikens <emily.c.aikens@gmail.com>
2022-08-11 19:53:11 -0700

    update image names in js

--- main.js
+++ main.js
@@ -6,8 +6,8 @@ let vars = {
     matches: 0, //counts total matches throughout the game
     curCards: [], //used to log the innerHTML of the cards, two at a time
     min: 5, //how many minutes of game play
-    cardImages: ["images/1.png", "images/3.png", "images/4.png", "images/6.png", "images/8.png", "images/11.png", "images/12.png", "images/14.png", 
-                "images/1.png", "images/3.png", "images/4.png", "images/6.png", "images/8.png", "images/11.png", "images/12.png", "images/14.png"]
+    cardImages: ["images/1.png", "images/2.png", "images/3.png", "images/4.png", "images/5.png", "images/6.png", "images/7.png", "images/8.png", 
+                "images/1.png", "images/2.png", "images/3.png", "images/4.png", "images/5.png", "images/6.png", "images/7.png", "images/8.png"]
 };
 
 const domSelect = {
